10686e205b6e5c6328507045ced64e7a2b6b79a1
rchui <ryan.w.chui@gmail.com>
2017-02-18 18:11:27 -0600

    arrays and pointers

---
+++ array.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	double array[3];
+	array[0]
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "add.h"
+
+int add(int x, int y);
+
+int main() {
+	std::cout << "The sumer of 3 and 4 is " << add(3, 4) << std::endl;
+	return 0;
+}
